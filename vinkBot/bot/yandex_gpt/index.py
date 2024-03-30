5927c1b083ed1ae09b049d113ddb2be00529add5
vlad3069 <vlad9603@gmail.com>
2024-03-30 18:24:22 +0300

    update index

--- vinkBot/bot/yandex_gpt/index.py
+++ vinkBot/bot/yandex_gpt/index.py
@@ -10,7 +10,7 @@ def get_gpt_response(input_data):
     yandex_api_key = os.getenv('YANDEX_API_KEY')
     folder_id = os.getenv('FOLDER_ID')
 
-    with open('./body.json', 'r', encoding='utf-8') as f:
+    with open('bot/yandex_gpt/body.json', 'r', encoding='utf-8') as f:
         data = json.dumps(json.load(f))
     response = requests.post(
         url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion',
