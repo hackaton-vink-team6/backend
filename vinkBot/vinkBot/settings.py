8cb69373f275e8f96a73545f797a65a832c404fa
vlad3069 <vlad9603@gmail.com>
2024-04-08 09:18:12 +0300

    settings edit

--- vinkBot/vinkBot/settings.py
+++ vinkBot/vinkBot/settings.py
@@ -6,23 +6,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 env = environ.Env()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$g=es1=$ne&zpviv$&rnc2+91xqrk&5-@no&bjqtfq9c&81*=('
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'daphne',
@@ -72,9 +64,6 @@ import bot.routing
 WSGI_APPLICATION = 'vinkBot.wsgi.application'
 ASGI_APPLICATION = "vinkBot.asgi.application"
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 
 if DEBUG:
     DATABASES = {
@@ -97,16 +86,12 @@ else:
         }
     }
 
-# Django Channels
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -124,9 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
-
 LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
@@ -136,13 +118,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
